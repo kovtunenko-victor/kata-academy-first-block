2cf26d6b00fcb9d034c272d663423c17e547e31b
Kovtunenko Victor <kovtunenko.victor@gmail.com>
2022-10-10 22:49:24 +0300

    Add implement hibernate dao by hibernate API

--- src/main/java/ru/kata/academy/kovtunenko/first/block/dao/HibernateUserDao.java
+++ src/main/java/ru/kata/academy/kovtunenko/first/block/dao/HibernateUserDao.java
@@ -1,35 +1,89 @@
 package ru.kata.academy.kovtunenko.first.block.dao;
 
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 import ru.kata.academy.kovtunenko.first.block.model.User;
+import ru.kata.academy.kovtunenko.first.block.util.Util;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class HibernateUserDao implements UserDao {
+    private static final Logger LOGGER = Logger.getLogger(HibernateUserDao.class.getName());
+
     public HibernateUserDao() {
 
     }
 
     public void createUsersTable() {
-
+        executeSql("CREATE TABLE users (id BIGINT NOT NULL AUTO_INCREMENT, name VARCHAR(255), last_name VARCHAR(255), age TINYINT, PRIMARY KEY (id)) AUTO_INCREMENT = 1");
     }
 
     public void dropUsersTable() {
-
+        executeSql("DROP TABLE users");
     }
 
     public void saveUser(String name, String lastName, byte age) {
+        Transaction tran = null;
 
+        try (Session session = Util.getOpenSession()) {
+            tran = session.beginTransaction();
+            session.save(new User(name, lastName, age));
+            tran.commit();
+        } catch (Exception ex) {
+            if (tran != null) {
+                tran.rollback();
+            }
+
+            LOGGER.log(Level.SEVERE, "Exception when save user", ex);
+        }
     }
 
     public void removeUserById(long id) {
+        Transaction tran = null;
 
+        try (Session session = Util.getOpenSession()) {
+            User user = session.get(User.class, id);
+            tran = session.beginTransaction();
+            session.delete(user);
+            tran.commit();
+        } catch (Exception ex) {
+            if (tran != null) {
+                tran.rollback();
+            }
+
+            LOGGER.log(Level.SEVERE, "Exception when remove user by id", ex);
+        }
     }
 
     public List<User> getAllUsers() {
-        return null;
+        try (Session session = Util.getOpenSession()) {
+            return session.createQuery("from User", User.class).getResultList();
+        } catch (Exception ex) {
+            LOGGER.log(Level.SEVERE, "Exception when get users list", ex);
+            return Collections.emptyList();
+        }
     }
 
     public void cleanUsersTable() {
+        executeSql("TRUNCATE TABLE users");
+    }
 
+    private void executeSql(String sql) {
+        Transaction tran = null;
+
+        try (Session session = Util.getOpenSession()) {
+            tran = session.beginTransaction();
+            session.createSQLQuery(sql).executeUpdate();
+            tran.commit();
+        } catch (Exception ex) {
+            if (tran != null) {
+                tran.rollback();
+            }
+
+            LOGGER.log(Level.SEVERE, "Exception when execute native sql [" + sql + "]", ex);
+        }
     }
 }
--- src/main/java/ru/kata/academy/kovtunenko/first/block/util/Util.java
+++ src/main/java/ru/kata/academy/kovtunenko/first/block/util/Util.java
@@ -1,8 +1,18 @@
 package ru.kata.academy.kovtunenko.first.block.util;
 
+import org.hibernate.SessionFactory;
+
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.Session;
+import org.hibernate.service.ServiceRegistry;
+import ru.kata.academy.kovtunenko.first.block.model.User;
+
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.DriverManager;
+import java.util.Properties;
 
 public class Util {
     private static final String SERVER_ADDRESS = "31.31.199.29";
@@ -11,9 +21,33 @@ public class Util {
     private static final String USER = "root";
     private static final String PASSWORD = "reporter2015";
 
+    private static SessionFactory sessionFactory;
+
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(
                 String.format("jdbc:mysql://%s:%s/%s", SERVER_ADDRESS, SERVER_PORT, DATA_BASE),
                 USER, PASSWORD);
+    }
+
+    public static Session getOpenSession() {
+        if (sessionFactory == null) {
+            Configuration cfg = new Configuration();
+            Properties prop = new Properties();
+            prop.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+            prop.put(Environment.URL, String.format("jdbc:mysql://%s:%s/%s", SERVER_ADDRESS, SERVER_PORT, DATA_BASE));
+            prop.put(Environment.USER, USER);
+            prop.put(Environment.PASS, PASSWORD);
+            prop.put(Environment.SHOW_SQL, "false");
+            prop.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+            prop.put(Environment.HBM2DDL_AUTO, "none");
+
+            cfg.setProperties(prop);
+            cfg.addAnnotatedClass(User.class);
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                    .applySettings(cfg.getProperties()).build();
+            sessionFactory = cfg.buildSessionFactory(serviceRegistry);
+        }
+
+        return sessionFactory.openSession();
     }
 }
